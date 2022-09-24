1565325944efc5de12b5b95955d3c9ae5276590b
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-09-24 02:57:22 +0200

    edit

--- src/Components/EditUser.js
+++ src/Components/EditUser.js
@@ -6,7 +6,9 @@ import withRouter from "./withRouter";
 
 
  class Edit extends React.Component{
-     state = { user:''
+     state = { user : [],
+       update:''
+      
     };
     componentDidMount() {
         let id =this.props.params.id
@@ -46,51 +48,51 @@ import withRouter from "./withRouter";
     // }
   
 
-    //stock value in state
-// changeHandler=(e) => {
-//     this.setState({ [e.target.id]: e.target.value})
+    // stock value in state
+changeHandler=(e) => {
+    this.setState({ update: e.target.value})
     
-//     // console.log(this.state)
-// }   
+    console.log(this.state.update)
+}   
 
 // send data to api json-server
-// submitHandler = (e) => {
-//     e.preventDefault() 
-//     axios.post('http://localhost:4000/user', this.state)
-//  // auto reload page     
-//     window.location.reload(false);
+submitHandler = (e) => {
+    e.preventDefault() 
+    axios.post('http://localhost:4000/user', this.state)
+ // auto reload page     
+    window.location.reload(false);
        
-// }
+}
 
 render() {
     
    
+    const { Name, Phone, Email } = this.state.user;
    
     return(
-
 
         
         <div className="container">
             <div className="row">
                 <div className="col-md-7">
                     <h1>Add User Form</h1>
-                    {this.state.user.map(user => (
+                    
                     <form >
-                        <div className="form-group">
+                        <div className="form-group" onSubmit={()=>this.submitHandler}>
                             <label htmlFor="Name">Full Name</label>
-                            <input type="text" className="form-control" defaultValue={""} id="Name" placeholder="Enter Name"></input>
+                            <input type="text" className="form-control" defaultValue={Name} id="Name" placeholder="Enter Name" onChange={this.changeHandler}></input>
                         </div>
                         <div className="form-group">
                             <label htmlFor="Phone">Phone number</label>
-                            <input type="text" className="form-control" id="Phone"    placeholder="Enter Number"></input>
+                            <input type="text" className="form-control" id="Phone"   defaultValue={Phone}  placeholder="Enter Number" onChange={()=>this.changeHandler}></input>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="Email" aria-describedby="emailHelp"  placeholder="Enter email"></input>
+                            <input type="email" className="form-control" id="Email"  defaultValue={Email} aria-describedby="emailHelp"  placeholder="Enter email" onChange={()=>this.changeHandler}></input>
                         </div>
                         <button type="submit" style={{marginTop:'20px',marginBottom:'20px'}} className="btn btn-primary">Submit</button>
                     </form>
-                         ))}
+                        
                 </div>
             </div>
         </div>
